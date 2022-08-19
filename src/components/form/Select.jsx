69a8c92887802deb7f780985750c359b21ee71b9
Daniel Rotstein Gonçalves <rotsteindaniel@gmail.com>
2022-08-19 05:57:11 -0300

    apiconnection

--- src/components/form/Select.jsx
+++ src/components/form/Select.jsx
@@ -4,9 +4,14 @@ const Select = ({ text, name, options, handleOnChange, value }) => {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}:</label>
-    <select name={name} id={name}>
+      <select name={name} id={name}>
         <option>Selecione uma opção</option>
-    </select>
+        {options.map((option) => {
+          <option value={option.id} key={option.id}>
+            {option.name}
+          </option>;
+        })}
+      </select>
     </div>
   );
 };
